e916869fd6cf40a457ef255b42ad38067752f1b1
Esa Koskinen <esa.t.koskinen@aalto.fi>
2016-02-19 19:25:44 +0200

    Made a stub for models.py

--- gamestore/gamestore/models.py
+++ gamestore/gamestore/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
-class PersonTest(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+class Game(models.Model):
+    name = models.CharField(max_length=50)
+    URL = models.URLfield(max_length=100)
+    description = models.TextField() # TODO: Think about how to implement line feeds.
+    # TODO: Other fields, e.g. link to dev, fk to dev, price, etc.
+    
+class GameSave(models.Model):
+    data = models.TextField() # JSON data about the saved game.
+    # TODO: Other fields, e.g. fk to game and user.
+    
+class Highscore(models.Model):
+    data = models.TextField() # Score in JSON format.
+    # TODO: Other fields, e.g. fk to game and user.
+    
+class GamesOwned(models.Model):
+    paymentState = models.TextField() # Payment state in JSON???
+    # TODO: Other fields, e.g. fk to game, user
