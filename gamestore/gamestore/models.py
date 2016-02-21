9692b2763a50629938dee3c00b85d7b4fbfc5305
Esa Koskinen <esa.t.koskinen@aalto.fi>
2016-02-21 15:18:27 +0200

    Added foreign keys; needs testing

--- gamestore/gamestore/models.py
+++ gamestore/gamestore/models.py
@@ -1,19 +1,43 @@
 from django.db import models
 
+# TODO: Users should have a ManyToManyField with GamesOwned !!!
+
 class Game(models.Model):
     name = models.CharField(max_length=50)
-    URL = models.URLfield(max_length=100)
-    description = models.TextField() # TODO: Think about how to implement line feeds.
-    # TODO: Other fields, e.g. link to dev, fk to dev, price, etc.
+    URL = models.URLfield(max_length=150)
+    description = models.TextField(blank=True) # TODO: Think about how to implement line feeds.
+    developer = models.ForeignKey(
+        'User', 
+        on_delete=models.SET_NULL, 
+        null=True)
+    price = models.DecimalField(max_digits=None, decimal_places=2)
     
 class GameSave(models.Model):
-    data = models.TextField() # JSON data about the saved game.
-    # TODO: Other fields, e.g. fk to game and user.
+    data = models.TextField(blank=True) # JSON data about the saved game.
+    game = models.ForeignKey(
+        'Game',
+        on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE)
     
 class Highscore(models.Model):
     data = models.TextField() # Score in JSON format.
-    # TODO: Other fields, e.g. fk to game and user.
+    game = models.ForeignKey(
+        'Game',
+        on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE)
     
 class GamesOwned(models.Model):
     paymentState = models.TextField() # Payment state in JSON???
-    # TODO: Other fields, e.g. fk to game, user
+    game = models.ForeignKey(
+        'Game',
+        on_delete=models.CASCADE) 
+    # Should users know about owned games which have been deleted?
+    # If so, the on_delete value for the fk should be models.SET_NULL
+    
+
+
+
