d4e2ab4fdd8272637955aadee23695306ec4b11c
koskine6 <esa.t.koskinen@aalto.fi>
2016-02-25 22:11:46 +0200

    Imported user to models

--- gamestore/gamestore/models.py
+++ gamestore/gamestore/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class UserExtension(models.Model):
     user = models.OneToOneField(
-        'User', 
+        User, 
         on_delete=models.CASCADE, 
         parent_link=False)
     isDeveloper = models.BooleanField(default=False)
@@ -16,7 +17,7 @@ class Game(models.Model):
     image = models.URLField(blank=True)
     image2 = models.URLField(blank=True)
     developer = models.ForeignKey(
-        'User', 
+        User, 
         on_delete=models.SET_NULL, 
         null=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
@@ -24,25 +25,25 @@ class Game(models.Model):
 class GameSave(models.Model):
     data = models.TextField(blank=True) # JSON data about the saved game.
     game = models.ForeignKey(
-        'Game',
+        Game,
         on_delete=models.CASCADE)
     user = models.ForeignKey(
-        'User',
+        User,
         on_delete=models.CASCADE)
     
 class Highscore(models.Model):
     data = models.TextField() # Score in JSON format.
     game = models.ForeignKey(
-        'Game',
+        Game,
         on_delete=models.CASCADE)
     user = models.ForeignKey(
-        'User',
+        User,
         on_delete=models.CASCADE)
     
 class GamesOwned(models.Model):
     paymentState = models.TextField() # Payment state in JSON???
     game = models.ForeignKey(
-        'Game',
+        Game,
         on_delete=models.CASCADE) 
     # Should users know about owned games which have been deleted?
     # If so, the on_delete value for the fk should be models.SET_NULL
