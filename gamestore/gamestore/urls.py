6b7c950555ccbc984e206c69f30dd86c73848860
AtteJaantila <atte.jaantila@aalto.fi>
2016-03-01 22:15:15 +0200

    Added logout possibility

--- gamestore/gamestore/urls.py
+++ gamestore/gamestore/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^register$', views.register),
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url(r'^logout$', 'django.contrib.auth.views.logout'),
     url(r'^user/([0-9a-zA-Z]+)$', views.userPage),
     url(r'^developer/([0-9a-zA-Z]+)$', views.developerPage),
     url(r'^game/([0-9]+)$', views.gameView), # TODO: Change number to the name of the game
