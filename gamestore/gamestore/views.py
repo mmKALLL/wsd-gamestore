18a258e10e77d1a1ea676119fb5be373d717c582
AtteJaantila <atte.jaantila@aalto.fi>
2016-03-03 23:28:46 +0200

    Always pass unspecified genre in views, broadened acceptable gamepage urls

--- gamestore/gamestore/views.py
+++ gamestore/gamestore/views.py
@@ -80,7 +80,7 @@ def gamePlayView(request, game_id):
 def gameList(request):
 	user = request.user
 	games = Game.objects.filter(isPublic=True)
-	genres = []
+	genres = ['Unspecified']
 	for game in games:
 		if game.genre not in genres:
 			genres.append(game.genre)
